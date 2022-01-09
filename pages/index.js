7cf1fe1b5c5c77ed14290cfc9911804277bad91f
pavan koka <pavan.koka@toppr.com>
2022-01-10 02:19:31 +0530

    changes

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "./index.module.scss";
-
-function Index() {
-    return <h1>Welcome aboard!</h1>;
-}
-
-export default Index;
