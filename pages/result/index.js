f7ba46d11659c2d8cd6589567304681ca8c3808a
pavan koka <pavan.koka@toppr.com>
2022-01-19 12:38:00 +0530

    changes

--- pages/result/index.js
+++ pages/result/index.js
@@ -3,9 +3,38 @@ import styles from "./index.module.scss";
 
 function Result() {
     const [result, setResult] = useState({});
+    const [sortedKeys, setSortedKeys] = useState([]);
+    console.log(sortedKeys, result);
+
     useEffect(() => {
         onMount();
     }, []);
+
+    function getTime(str) {
+        const minsI = str.search(/mins/);
+        const secsI = str.search(/secs/);
+        const mins = parseInt(str.substring(0, minsI), 10);
+        const secs = parseInt(str.substring(minsI + 5, secsI), 10);
+
+        return mins * 60 + secs;
+    }
+
+    function sortKeys(result) {
+        const keys = Object.keys(result);
+        keys.sort(function (a, b) {
+            const aTime = getTime(result[a].time);
+            const bTime = getTime(result[b].time);
+
+            if (aTime < bTime) {
+                return -1;
+            } else if (aTime > bTime) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+        setSortedKeys(keys);
+    }
 
     const onMount = async () => {
         const res = await fetch(
@@ -19,14 +48,13 @@ function Result() {
         );
         res.json().then((result) => {
             setResult(result);
+            sortKeys(result);
         });
     };
 
-    const keys = Object.keys(result);
-
     return (
         <div className={styles.wrapper}>
-            {keys.map((key) => (
+            {sortedKeys.map((key) => (
                 <div key={key} className={styles.row}>
                     <p className={styles.name}>
                         {result[key].name || "not logged"}
